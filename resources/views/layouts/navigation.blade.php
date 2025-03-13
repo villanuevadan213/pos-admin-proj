2686fb5168282188404d7c20d31bf1cbddbf5f4b
Dan Louie <villanuevadan213@gmail.com>
2025-03-13 21:43:10 +0800

    Add login and landing page routes, update guest layout

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -3,10 +3,10 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <!-- Logo -->
+                <!-- Logo Test -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <img src="{{ asset('common/images/logo_small.png') }}" alt="Logo">
                     </a>
                 </div>
 
